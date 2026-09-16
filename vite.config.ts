e3e4afd8a355fe90ea3a059e79f32facdf0f2f28
Enock Mumo <164017139+Dream-kid-342@users.noreply.github.com>
2026-09-16 19:06:18 +0300

    fixed issues

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,7 @@ export default defineConfig({
     // nitro/vite builds from this
     server: { entry: "server" },
   },
+  nitro: {
+    preset: process.env.NITRO_PRESET || "node-server",
+  },
 });
